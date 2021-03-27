89b8687c72bb09a9f7cacb2f2ec50ee709704165
Brandon Sundell <brandon.sundell@icloud.com>
2021-03-26 17:36:28 -0700

    Updated and built Database

---
+++ SunnyDaysSports/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SunnyDaysSports.Models;
+
+namespace SunnyDaysSports.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IStoreRepository repository;
+
+        public HomeController(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+        public IActionResult Index()
+        {
+            return View(repository.Products);
+        }
+    }
+}
